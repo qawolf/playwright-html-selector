418c43437f1afb7232a05662bf9ade63fa07e9c8
Jon Perl <perl.jonathan@gmail.com>
2020-03-08 17:46:38 -0600

    make htmlToElement support root elements like html

--- src/web/htmlToElement.ts
+++ src/web/htmlToElement.ts
@@ -1,7 +1,24 @@
-export const htmlToElement = (html: string): HTMLElement => {
-  const template = document.createElement('template');
-  html = html.trim(); // remove whitespace
-  template.innerHTML = html;
+export const getTags = (html: string): string[] => html.match(/(<([^>]+)>)/g);
 
-  return template.content.firstChild as HTMLElement;
+export const getTagName = (tag: string): string => {
+  return tag.replace(/<|>|\//g, '').trim();
+};
+
+export const htmlToElement = (html: string): HTMLElement => {
+  const tags = getTags(html);
+  const tagName = getTagName(tags[0]);
+
+  const element = document.createElement(tagName);
+
+  // remove the opening tag
+  let innerHtml = html.substring(tags[0].length);
+  if (tags.length > 1) {
+    // remove the closing tag
+    const closingTag = tags[tags.length - 1];
+    innerHtml = innerHtml.substring(0, innerHtml.length - closingTag.length);
+  }
+
+  element.innerHTML = innerHtml;
+
+  return element;
 };
